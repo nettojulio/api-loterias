a516533f886e08f74b23b3ea0fb83e75844c70e7
Julio Neto <nettojulio@hotmail.com>
2023-04-26 21:21:30 -0300

    refactor service

--- src/resultados/resultados.service.ts
+++ src/resultados/resultados.service.ts
@@ -14,17 +14,20 @@ export class ResultadosService {
 
   async ultimoResultadoLoteriaEscolhida(loteria: string) {
     const cacheKey = `resultado:${loteria}:latest`;
+    const cache = await this.getCache(cacheKey);
 
-    let value = await this.cacheManager.get(cacheKey);
-    if (!value) {
-      this.logger.log('Obtendo dados via HTTPS');
-      value = await this.resultadosClient.ultimoResultadoLoteriaEscolhida(
-        loteria,
-      );
-      this.logger.log(`Resultado registrado em cache: ${cacheKey}`);
-      await this.cacheManager.set(cacheKey, value);
+    if (cache) {
+      return cache;
     }
-    return value;
+
+    this.logger.log('Obtendo dados via HTTPS');
+
+    const resultado =
+      await this.resultadosClient.ultimoResultadoLoteriaEscolhida(loteria);
+
+    await this.setCache(cacheKey, resultado);
+
+    return resultado;
   }
 
   async ultimoResultadoLoteriaEConcursoEscolhido(
@@ -32,38 +35,72 @@ export class ResultadosService {
     concurso: number,
   ) {
     const cacheKey = `resultado:${loteria}:${concurso}:latest`;
+    const cache = await this.getCache(cacheKey);
 
-    let value = await this.cacheManager.get(cacheKey);
-    if (!value) {
-      this.logger.log('Obtendo dados via HTTPS');
-      value =
-        await this.resultadosClient.ultimoResultadoLoteriaEConcursoEscolhido(
-          loteria,
-          concurso,
-        );
-      this.logger.log(`Resultado registrado em cache: ${cacheKey}`);
-      await this.cacheManager.set(cacheKey, value);
+    if (cache) {
+      return cache;
     }
-    return value;
+
+    this.logger.log('Obtendo dados via HTTPS');
+    const resultado =
+      await this.resultadosClient.ultimoResultadoLoteriaEConcursoEscolhido(
+        loteria,
+        concurso,
+      );
+
+    await this.setCache(cacheKey, resultado);
+
+    return resultado;
   }
 
   async ultimoResultadoTodosOsConcursos() {
     const cacheKey = 'resumo:latest';
 
-    let values: any = await this.cacheManager.get(cacheKey);
+    const cache = await this.getCache(cacheKey);
 
-    if (!values) {
-      this.logger.log('Obtendo dados via HTTPS');
-      values = await this.resultadosClient.ultimoResultadoTodosOsConcursos();
-      this.logger.log(`Resultado registrado em cache: ${cacheKey}`);
-      await this.cacheManager.set(cacheKey, values);
-
-      for (const value in values) {
-        const cacheKeyValue = `resumo:${value}:latest`;
-        await this.cacheManager.set(cacheKeyValue, values[value]);
-      }
+    if (cache) {
+      return cache;
     }
 
-    return values;
+    this.logger.log('Obtendo dados via HTTPS');
+    const resultado =
+      await this.resultadosClient.ultimoResultadoTodosOsConcursos();
+
+    await this.setCache(cacheKey, resultado);
+
+    for (const value in resultado) {
+      const cacheKeyValue = `resumo:${value}:latest`;
+      await this.setCache(cacheKeyValue, resultado[value]);
+    }
+
+    return resultado;
+  }
+
+  async multiplosResultados(loteria: string, concurso: number) {
+    const concursosList: Array<number> = [];
+
+    for (let i = concurso; i <= concurso + 29; i++) {
+      concursosList.push(i);
+    }
+
+    return await Promise.all(
+      concursosList.map(async item => {
+        return await this.resultadosClient.ultimoResultadoLoteriaEConcursoEscolhido(
+          loteria,
+          item,
+        );
+      }),
+    );
+  }
+
+  private async getCache(key: string) {
+    const data = await this.cacheManager.get(key);
+    this.logger.log('Verificando cache: ' + (data ? 'HIT' : 'MISS'));
+    return data;
+  }
+
+  private async setCache(key: string, value: any) {
+    this.logger.log(`Resultado registrado em cache: ${key}`);
+    await this.cacheManager.set(key, value);
   }
 }

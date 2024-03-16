96470d63c7cb16a27fcc56a6a0f1f0ddf96ca273
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2024-03-16 10:42:52 -0300

    semana5 nova branch

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
